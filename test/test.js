a42e6e9def143413f3f96d1b885dcca0b7701186
2ssue <e2ssue@gmail.com>
2020-04-13 17:33:30 +0900

    Test: 로그인 후 세션이 유지되는지 테스트하는 코드 추가

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ const request = require('supertest');
 const { parseCookies } = require('../utils/utils.js');
 
 describe('로그인 테스트', () => {
+  let cookies;
+
   it('로그인이 진행되고 세션 쿠키가 등록됨', () => {
     request(app)
       .post('/user')
@@ -14,8 +16,18 @@ describe('로그인 테스트', () => {
       .end((err, res) => {
         assert.equal(res.body.result, 'success');
 
-        const cookies = parseCookies(res.headers['set-cookie'].pop());
+        cookies = parseCookies(res.headers['set-cookie'].pop());
         assert.ok(cookies.loginSession);
+      });
+  });
+
+  it('세션 유지 테스트', () => {
+    request(app)
+      .get('/user')
+      .set('Cookie', [`loginSession=${cookies.loginSession}`])
+      .expect(200)
+      .end((err, res) => {
+        assert.equal(res.body.result, 'find');
       });
   });
 });
