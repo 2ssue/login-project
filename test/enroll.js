3ca91bac3f3eb80780b042f8fbd94ce260c2c979
2ssue <e2ssue@gmail.com>
2020-04-13 18:02:42 +0900

    Rename: 테스트 코드 폴더링
    - 테스트 코드를 별도로 실행 할 수 있도록 폴더링함

---
+++ test/enroll.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const app = require('../app.js');
+const request = require('supertest');
+
+describe('회원가입 테스트', () => {
+  const newUser = {
+    userid: 'user01',
+    password: 'Password123!',
+    name: 'user01',
+    year: 2020,
+    month: 04,
+    date: 13,
+    gender: '여자',
+    email: 'user01@user.com',
+    phone: '01012345678',
+    hobby: '코딩,코딩,코딩',
+  };
+
+  it('새로운 유저가 등록됨', () => {
+    request(app)
+      .post('/enroll')
+      .set('Accept', 'application/json')
+      .send(newUser)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        assert.equal(res.body.result, 'success');
+      });
+  });
+
+  it('이미 있는 유저는 등록되지 않음', () => {
+    request(app)
+      .post('/enroll')
+      .set('Accept', 'application/json')
+      .send(newUser)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        assert.equal(res.body.result, 'fail');
+      });
+  });
+});
