cfc51db14ed5db0483485c6a692290ede1db5c93
2ssue <e2ssue@gmail.com>
2020-04-14 20:08:12 +0900

    Fix: 로그아웃 후 로그인을 하면 새로고침이 발생하는 현상
    - 이벤트 등록이 사라져서 발생한 이슈
    - 회원가입과 동일하게 observer를 활용해 이벤트가 재 등록되도록 함

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,10 +1,23 @@
 import { login } from './routing.js';
 
-(() => {
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if ('로그인' === mutation.addedNodes[0].innerText) onlogin();
+  });
+});
+
+const config = {
+  childList: true,
+};
+
+observer.observe(document.querySelector('main'), config);
+onlogin();
+
+function onlogin() {
   const loginButton = document.querySelector('button');
   loginButton.addEventListener('click', (event) => {
     event.preventDefault();
 
     login();
   });
-})();
+}
