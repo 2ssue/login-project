073c74606bec37a571a99b0f62546670775e53e3
2ssue <e2ssue@gmail.com>
2020-04-20 23:25:12 +0900

    Fix: key는 있으나 value가 없는 경우에 대한 에러처리 추가

--- public/javascripts/utils/utils.js
+++ public/javascripts/utils/utils.js
@@ -1,7 +1,9 @@
 const parseCookies = (cookies) => {
   return cookies.split(';').reduce((acc, cookie) => {
     const [key, value] = cookie.split('=');
-    acc[decodeURIComponent(key.trim())] = decodeURIComponent(value.trim());
+    if (key && value) {
+      acc[decodeURIComponent(key.trim())] = decodeURIComponent(value.trim());
+    }
     return acc;
   }, {});
 };
